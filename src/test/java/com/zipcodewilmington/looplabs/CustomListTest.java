6cdeea0a8bdedf089dccffb9415d76a94e881d98
James Coates <jamescoates@zipcoders-MacBook-Pro.local>
2019-03-06 15:52:28 -0500

    Tests working

--- src/test/java/com/zipcodewilmington/looplabs/CustomListTest.java
+++ src/test/java/com/zipcodewilmington/looplabs/CustomListTest.java
@@ -3,35 +3,40 @@ package com.zipcodewilmington.looplabs;
 import org.junit.Assert;
 import org.junit.Test;
 
-// public class CustomListTest {
-//    @Test
-//    public void removeAllTest(){
-//        CoatesList jimlist = new
-//        Object[] before = new Object[]{1,1,2,2,2,3,3,3,4,4,4};
-//        Object[] actual = CoatesList.removeAll(before,3);
-//
-//        Object[] expected = new Object[]{1,1,2,2,2,4,4,4};
-//
-//        Assert.assertArrayEquals(expected,actual);
-//    }
-//
-//    @Test
-//    public void getNumberOccurencesTest(){
-//        Object[] before = new Object[]{1,1,2,2,2,3,3,3,4,4,4};
-//        Integer expected = 3;
-//        Integer actual = CoatesList.getNumberOfOccurences(before, before[4]);
-//        Assert.assertEquals(expected, actual);
-//
-//
-//    }
-//
-//    @Test
-//    public void removeAtIndexTest(){
-//        Object[] before = new Object[]{1,1,2,2,2,3,3,3,4,4,4};
-//        Object[] expected = new Integer[] {1,1,2,2,3,3,3,4,4,4};
-//        Object [] actual = CoatesList.removeAtIndex(before,3);
-//
-//        Assert.assertArrayEquals(expected,actual);
-//    }
-//
-//}
+ public class CustomListTest {
+    @Test
+    public void removeAllTest(){
+        Object[] before = new Object[]{1,1,2,2,2,3,3,3,4,4,4};
+        CoatesList coatesList = new CoatesList(before);
+        coatesList.removeAll(3);
+        Object[] actual = coatesList.toArray();
+
+        Object[] expected = new Object[]{1,1,2,2,2,4,4,4};
+
+        Assert.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void getNumberOccurencesTest(){
+        Object[] before = new Object[]{1,1,2,2,2,3,3,3,4,4,4};
+        CoatesList coatesList = new CoatesList(before);
+        Integer expected = 3;
+        Integer actual = coatesList.getNumberOfOccurences(before[4]);
+        Assert.assertEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void removeAtIndexTest(){
+        Object[] before = new Object[]{1,1,2,2,2,3,3,3,4,4,4};
+        CoatesList coatesList = new CoatesList(before);
+
+        Object[] expected = new Integer[] {1,1,2,2,3,3,3,4,4,4};
+        coatesList.removeAtIndex(3);
+        Object[] actual = coatesList.toArray();
+
+        Assert.assertArrayEquals(expected,actual);
+    }
+
+}
